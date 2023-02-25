670ed06d4c1314d3d3b68197c5f9e93ec1d5ab14
Jade Wei <77248838+jadew33@users.noreply.github.com>
2023-02-24 21:08:01 -0500

    compress images and minor mobile edits

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,6 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <a id="top"></a> */}
       <Navbar />
       <Hero />
       <About />
